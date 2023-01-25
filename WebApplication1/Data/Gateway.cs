9f227735254beb01f4df1761c6e1f84a8967d946
Jakub Sedlak <sed0211@vsb.cz>
2023-01-25 19:16:44 +0100

    new models for app

--- WebApplication1/Data/Gateway.cs
+++ WebApplication1/Data/Gateway.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApplication1.Data
+﻿using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
 {
+    //Creating  gateway class to serialize Invoice class from models into JSON
     public class Gateway
     {
+        public void SerializeToJson(Invoice invoice, string filePath)
+        {
+            try
+            {
+                using (var stream = File.Create(filePath))
+                using (var writer = new StreamWriter(stream))
+                {
+                    var json = JsonConvert.SerializeObject(invoice);
+                    writer.Write(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                // handle exception
+                Console.WriteLine("An error occurred while writing the JSON file: " + ex.Message);
+            }
+        }
+
+        public Invoice DeserializeFromJson(string filePath)
+        {
+            using (var stream = File.OpenRead(filePath))
+            using (var reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Invoice>(json);
+            }
+        }
     }
 }
